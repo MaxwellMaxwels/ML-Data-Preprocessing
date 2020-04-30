316da29e09068db0159757f6848acdfab1c32614
Maxwell Ochieng <maxwellmaxwels@gmail.com>
2020-04-30 16:32:58 +0300

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -21,3 +21,5 @@ from sklearn.preprocessing import StandardScaler
 sc_x=StandardScaler()
 x_train=sc_x.fit_transform(x_train)
 x_test=sc_x.transform(x_test)
+sc_y=StandardScaler()
+y_train=sc_y.fit_transform(y_train)
