aee38ac9c52c87b7a2dbd87cc0c901e396f572ef
Maxwell Ochieng <maxwellmaxwels@gmail.com>
2024-01-28 23:15:01 +0300

    Update R_data_preprocessing.R

--- R_data_preprocessing.R
+++ R_data_preprocessing.R
@@ -7,6 +7,7 @@ dataset$Age=ifelse(is.na(dataset$Age),
 dataset$Salary=ifelse(is.na(dataset$Salary), 
                    ave(dataset$Salary, FUN=function(x) mean(x, na.rm=TRUE)), 
                    dataset$Salary)
+                      
 dataset$Country=factor(dataset$Country,
                        levels=c('France', 'Spain', 'Germany'), 
                        labels = c(1,2,3))
